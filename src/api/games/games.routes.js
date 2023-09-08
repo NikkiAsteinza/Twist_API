e5362d89a93c1eab6280a6df1a15635099a00647
Nikki A.A <nikki.asteinza@gmail.com>
2023-09-08 22:32:23 +0200

    game rountes and views

--- src/api/games/games.routes.js
+++ src/api/games/games.routes.js
@@ -2,7 +2,7 @@ const Game = require("./games.model");
 
 const gamesRoutes = require("express").Router();
 
-gamesRoutes.get("/games-list", async (req, res) => {
+gamesRoutes.get("/list", async (req, res) => {
     const games = await Game.find();
   
     res.render("games", { games });
