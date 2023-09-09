2a9fdfcc8140d3d2728a5bd8947ee0b8b0bbdb43
Nikki A.A <nikki.asteinza@gmail.com>
2023-09-09 21:47:46 +0200

    ci: added vercel file and other changes

--- src/api/games/games.routes.js
+++ src/api/games/games.routes.js
@@ -8,4 +8,10 @@ gamesRoutes.get("/list", async (req, res) => {
     res.render("games", { games });
 });
 
+gamesRoutes.get("/create", async (req, res) => {
+    const games = await Game.find();
+  
+    res.render("form/game", { games });
+});
+
 module.exports = gamesRoutes;
